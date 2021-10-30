a5e9d9a5843870e75cfc95b53f6a7a64b5ebe312
mahak2011 <mahak.raju20@gmail.com>
2021-10-31 03:47:25 +0530

    stores visited

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,7 +1,12 @@
-import React, { useContext } from 'react';
-import styled from 'styled-components';
-import './styles.scss';
-import { ClientContext } from '../../context';
+import React, { useContext } from "react";
+import styled from "styled-components";
+import "./styles.scss";
+import { ClientContext } from "../../context";
+import { Card, Avatar } from "antd";
+import Store1 from "../../assets/store1.jpg";
+import Store2 from "../../assets/store2.jpg";
+import Store3 from "../../assets/store3.jpg";
+import Store4 from "../../assets/store4.jpg";
 
 const Wrapper = styled.div`
   padding: 30px 20px 70px;
@@ -13,7 +18,7 @@ const Wrapper = styled.div`
 const SubHeading = styled.div`
   width: 100%;
   padding: 0 20px 30px;
-  font-family: 'Sora', sans-serif;
+  font-family: "Sora", sans-serif;
   font-size: 20px;
   font-weight: 400;
   margin: 0 auto 0;
@@ -23,7 +28,7 @@ const SubHeading = styled.div`
 const Heading = styled.div`
   width: 100%;
   padding: 10px 20px 20px;
-  font-family: 'Sora', sans-serif;
+  font-family: "Sora", sans-serif;
   font-size: 35px;
   font-weight: 900;
   margin: auto;
@@ -32,9 +37,106 @@ const Heading = styled.div`
 
 const HomePage = () => {
   const context = useContext(ClientContext);
+  const { Meta } = Card;
   return (
     <Wrapper>
       <Heading>Visited Stores</Heading>
+      <Card
+        style={{
+          width: "100%",
+          marginTop: 16,
+          borderRadius: "10px",
+          boxShadow: "5px 5px 5px 0px #bdbdbd",
+        }}
+      >
+        <Meta
+          avatar={
+            <Avatar
+              style={{
+                borderRadius: "10px",
+                border: "1px solid #bdbdbd",
+              }}
+              shape="square"
+              size={120}
+              src={Store1}
+            />
+          }
+          title="Carniceria"
+          description="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet"
+        />
+      </Card>
+      <Card
+        style={{
+          width: "100%",
+          marginTop: 16,
+          borderRadius: "10px",
+          boxShadow: "5px 5px 5px 0px #bdbdbd",
+        }}
+      >
+        <Meta
+          avatar={
+            <Avatar
+              style={{
+                borderRadius: "10px",
+                border: "1px solid #bdbdbd",
+              }}
+              shape="square"
+              size={120}
+              src={Store2}
+            />
+          }
+          title="Countdown"
+          description="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet"
+        />
+      </Card>
+      <Card
+        style={{
+          width: "100%",
+          marginTop: 16,
+          borderRadius: "10px",
+          boxShadow: "5px 5px 5px 0px #bdbdbd",
+        }}
+      >
+        <Meta
+          avatar={
+            <Avatar
+              style={{
+                borderRadius: "10px",
+                border: "1px solid #bdbdbd",
+              }}
+              shape="square"
+              size={120}
+              src={Store3}
+            />
+          }
+          title="Waitrose"
+          description="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet"
+        />
+      </Card>
+      <Card
+        style={{
+          width: "100%",
+          marginTop: 16,
+          borderRadius: "10px",
+          boxShadow: "5px 5px 5px 0px #bdbdbd",
+        }}
+      >
+        <Meta
+          avatar={
+            <Avatar
+              style={{
+                borderRadius: "10px",
+                border: "1px solid #bdbdbd",
+              }}
+              shape="square"
+              size={120}
+              src={Store4}
+            />
+          }
+          title="Shahi Grocer"
+          description="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet"
+        />
+      </Card>
     </Wrapper>
   );
 };
