b0109226d25881162227a920f06fb571411ab8c4
Apoorv Dwivedi <apoorvd14@gmail.com>
2021-10-31 01:50:47 +0530

    visited store page created

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import styled from 'styled-components';
 import './styles.scss';
+import { ClientContext } from '../../context';
 
-const Home = () => {
-    return (
-        <div>
-           <h1>Visited Stores</h1> 
-        </div>
-    )
-}
+const Wrapper = styled.div`
+  padding: 30px 20px;
+  background-color: #f5f5f5;
+  min-height: 100vh;
+  text-align: center;
+`;
 
-export default Home
+const SubHeading = styled.div`
+  width: 100%;
+  padding: 0 20px 30px;
+  font-family: 'Sora', sans-serif;
+  font-size: 20px;
+  font-weight: 400;
+  margin: 0 auto 0;
+  color: #a9a9a9;
+`;
+
+const Heading = styled.div`
+  width: 100%;
+  padding: 10px 20px 20px;
+  font-family: 'Sora', sans-serif;
+  font-size: 35px;
+  font-weight: 900;
+  margin: auto;
+  color: #2b2b2b;
+`;
+
+const HomePage = () => {
+  const context = useContext(ClientContext);
+  return (
+    <Wrapper>
+      <Heading>Visited Stores</Heading>
+    </Wrapper>
+  );
+};
+
+export default HomePage;
